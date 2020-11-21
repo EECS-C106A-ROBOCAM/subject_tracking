f7ec475b39fda1b4f0e12e78ae529bcaf7d33bfe
Jaiveer Singh <j.singh@berkeley.edu>
2020-11-21 15:31:39 -0800

    clean up build devel foldersg

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ajain23/ros_workspaces/lab5/src
-# Build directory: /home/ajain23/ros_workspaces/lab5/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(ik)
-subdirs(planning)
