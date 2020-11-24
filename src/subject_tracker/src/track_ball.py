078c81dc71a63c0522a444564751e8ddf378a7db
Jaiveer Singh <j.singh@berkeley.edu>
2020-11-23 16:48:02 -0800

    add images and test mode

--- src/subject_tracker/src/track_ball.py
+++ src/subject_tracker/src/track_ball.py
@@ -3,6 +3,9 @@
 
 import cv2
 import rospy
+import rospkg
+
+import numpy as np
 
 from cv_bridge import CvBridge
 
@@ -15,43 +18,47 @@ class BallTracker:
     FOCAL_LENGTH = 1
 
     def __init__(self):
-        self.subscriber = rospy.Subscriber("input/image_raw", Image, self.locate_ball, queue_size=1)
+        self.subscriber = rospy.Subscriber("input/image_raw", Image, self.locate_ball_callback, queue_size=1)
 
         self.publisher = rospy.Publisher("output/adjustment_pose", Pose, queue_size=1)
 
         self.bridge = CvBridge()
 
 
-    def locate_ball(self, msg):
+    def locate_ball_callback(self, msg):
         # print("callback")
-        image = self.bridge.imgmsg_to_cv2(msg, "mono8")
+        image = self.bridge.imgmsg_to_cv2(msg)
+        locate_ball(image)
 
-        blurred = cv2.medianBlur(image, 5)
+    def locate_ball(self, image):
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        blurred = cv2.medianBlur(gray, 5)
 
         rows = blurred.shape[0]
-        circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, 1, rows / 8,
-                                param1=100, param2=30,
-                                minRadius=1, maxRadius=30)
+
+        circles = cv2.HoughCircles(blurred,cv2.HOUGH_GRADIENT, 1,
+                            param1=50,param2=30,minRadius=0,maxRadius=0, minDist=50)
         # TODO(JS): tune the bounds on the Hough parameters
         
         if circles is not None:
             circles = np.uint16(np.around(circles))
 
-            target = circles[0]
+            target = circles[0][0]
+            print(target)
             center = (target[0], target[1])
             radius = target[2]
 
-            x_adj = center[0] - image.rows()
-            y_adj = center[1] - image.cols()
+            x_adj = center[0] - image.shape[0]
+            y_adj = center[1] - image.shape[1]
             z_adj = (radius - BallTracker.TARGET_RADIUS) / BallTracker.FOCAL_LENGTH
 
-            cv2.circle(image, center, 1, (100, 0, 100), 3)
-            cv2.circle(image, center, radius, (0, 255, 255), 3)
+            # cv2.circle(image, center, 1, (100, 0, 100), 3)
+            # cv2.circle(image, center, radius, (0, 255, 255), 3)
 
             # Also draw all circles
-            for i in circles[1, :]:
+            for i in circles[0, :]:
                 center = (i[0], i[1])
-                # circle center
+                # # circle center
                 cv2.circle(image, center, 1, (0, 100, 100), 3)
                 # circle outline
                 radius = i[2]
@@ -61,7 +68,9 @@ class BallTracker:
     
     
         cv2.imshow("detected circles", image)
-        cv2.waitKey(1)
+        key = cv2.waitKey(0) & 0xFF
+        if key == 27:
+           cv2.destroyAllWindows()
 
 
 
@@ -72,5 +81,12 @@ def main():
 
     rospy.spin()
 
+def test():
+    tracker = BallTracker()
+    images_path = rospkg.RosPack().get_path('subject_tracker')+"/images/"
+    image = cv2.imread(images_path + "test0.png")
+    tracker.locate_ball(image)
+
 if __name__ == "__main__":
-    main()
+    # main()
+    test()
