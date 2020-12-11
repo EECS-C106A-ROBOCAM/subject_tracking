37aaf5f8a6bf9866c3fcac9d299769a7886dbcc4
Jaiveer Singh <j.singh@berkeley.edu>
2020-12-11 07:19:03 -0800

    did the four bar stuff

--- src/ik_solver/src/CustomIKSolver.py
+++ src/ik_solver/src/CustomIKSolver.py
@@ -5,6 +5,7 @@ import rospy
 from tf_conversions import posemath
 import PyKDL
 import math
+import numpy as np
 
 from geometry_msgs.msg import (
     PoseStamped
@@ -68,13 +69,56 @@ def solveIK(targetPoseStamped):
     # 2. Complete:
     orangePoint = targetFrame.p - cameraYawToCameraDisplacement
     
-    print("Camera offset: {}".format(cameraFrame.p))
-    print("Camera Yaw offset: {}".format(cameraYawFrame.p))
-    print("CameraYaw-to-Camera Fixed XYZ displacement: {}".format(cameraYawToCameraDisplacement))
+    # print("Camera offset: {}".format(cameraFrame.p))
+    # print("Camera Yaw offset: {}".format(cameraYawFrame.p))
+    # print("CameraYaw-to-Camera Fixed XYZ displacement: {}".format(cameraYawToCameraDisplacement))
     print("Orange Point: {}".format(orangePoint))
     
 
-    print("Camera orientation: {}".format(cameraFrame.M.GetQuaternion()))
+    # print("Camera orientation: {}".format(cameraFrame.M.GetQuaternion()))
+
+    # 3. Convert orange point to cylindrical coordinates
+    orange_X, orange_Y, orange_Z = orangePoint
+    orange_R = math.sqrt(orange_X ** 2 + orange_Y ** 2)
+    orange_Theta = math.atan2(orange_Y, orange_X) # Theta measured from global positive X axis
+    # 3. Complete: (above)
+    print("Orange R: {} Theta: {}".format(orange_R, math.degrees(orange_Theta)))
+
+    # 4. Solve for J2 and J3 in the idealized R-Z plane
+    targetPointOrig_RZ = np.array([0.50, 0.12]) #np.array([orange_R, orange_Z])
+
+    # First, remove the fixed offsets from the wrist, elbow, and shoulder pieces
+    wristOffset_RZ = np.array([0.025, -0.045]) # TODO(JS): Get this from CAD/URDF
+    elbowOffset_RZ = np.array([0.035, 0.01])
+    shoulderOffset_RZ = np.array([-0.02, 0.12])
+
+    targetPoint_RZ = targetPointOrig_RZ - wristOffset_RZ - elbowOffset_RZ - shoulderOffset_RZ
+    print("Need to solve 2D 2-link IK from (0, 0) to ({}, {})".format(*targetPoint_RZ))
+
+    # The following steps use the same labels as the classic 2D planar IK derivation
+    a1, a2 = 0.3, 0.3
+    ik_x, ik_y = targetPoint_RZ
+    
+    q2_a = math.acos((ik_x ** 2 + ik_y ** 2 - a1 ** 2 - a2 ** 2) / (2 * a1 * a2))
+    q1_a = math.atan2(ik_y, ik_x) - math.atan2(a2 * math.sin(-q2_a), a1 + a2 * math.cos(-q2_a))
+
+    q2_b = -1 * math.acos((ik_x ** 2 + ik_y ** 2 - a1 ** 2 - a2 ** 2) / (2 * a1 * a2))
+    q1_b = math.atan2(ik_y, ik_x) + math.atan2(a2 * math.sin(q2_b), a1 + a2 * math.cos(q2_b))
+
+    # Choose 'better' set of q1_ab, q2_ab
+    q1, q2 = q1_a, q2_a # TODO(JS): Is this always the better one?
+
+    J2_initial = q1
+    J2_offset = math.radians(90) # J3's zero position is vertical, not horizontal
+    J2 = J2_initial - J2_offset
+
+    # Since we have a parallel link, the angle for J3 is not simply q2. Instead, use transversal
+    J3_initial = q1 - q2
+    J3_offset = math.radians(-60) # J2's zero position is below horizontal
+    J3 = J3_initial - J3_offset
+    # 4. Complete (above)
+    print("To reach simple (R, Z) = ({}, {}), set J2={} J3={}".format(targetPoint_RZ[0], targetPoint_RZ[1], math.degrees(J2), math.degrees(J3)))
+    
     
     
 
@@ -83,7 +127,7 @@ def testIK():
 
     """ CHANGE ME """
     pitch_deg, yaw_deg, roll_deg = 0, 0, 0
-    x, y, z = 0.0, 0.4, 0.2 # in meters
+    x, y, z = 0.2, 0.4, 0.2 # in meters
 
     targetVector = PyKDL.Vector(x, y, z)
 
